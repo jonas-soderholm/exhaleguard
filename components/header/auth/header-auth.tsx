ee991f15ab29e7e67419eea9e82c290a4a90ae99
jonas-soderholm <jonas.soderholm89@gmail.com>
2025-01-28 18:44:38 +0100

    fix: resolve eslint errors

--- components/header/auth/header-auth.tsx
+++ components/header/auth/header-auth.tsx
@@ -1,10 +1,4 @@
-import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import Link from "next/link";
-import { Badge } from "../../ui/badge";
-import { Button } from "../../ui/button";
 import { createClient } from "@/utils/supabase/server";
-import { ThemeSwitcher } from "../../ui/theme-switcher";
 
 export default async function AuthButton() {
   const supabase = await createClient();
