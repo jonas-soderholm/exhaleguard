701f06ba9cf730ea5fa2a89c586faa589772dc8a
jonas-soderholm <jonas.soderholm89@gmail.com>
2025-01-28 18:50:05 +0100

    fix: resolve eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,33 @@
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+//   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   pluginReact.configs.flat.recommended,
+// ];
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule to allow JSX without React import
+    },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
